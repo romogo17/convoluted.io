7c91f6ca13a76105dcd2d74fccf8c013fc4ab0ec
Roberto Mora <romogo17@gmail.com>
2023-11-19 11:11:18 -0600

    updates astro to v3

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,8 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-import image from "@astrojs/image";
-
 import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), image({
-    serviceEntryPoint: '@astrojs/image/sharp'
-  }), mdx()]
+  integrations: [tailwind(), mdx()]
 });
--- src/content/config.ts
+++ src/content/config.ts
@@ -2,14 +2,17 @@ import { z, defineCollection, reference } from 'astro:content';
 
 const postCollection = defineCollection({
     type: 'content',
-    schema: z.object({
+    schema: ({ image }) => z.object({
         isDraft: z.boolean().default(false),
 
         title: z.string(),
         description: z.string(),
         author: z.string().default('Roberto Mora'),
-        image: z.object({
-            src: z.string(),
+
+        cover: z.object({
+            image: image().refine((img) => img.width >= 1080, {
+                message: "Cover image must be at least 1080 pixels wide!",
+            }),
             alt: z.string(),
         }),
 
